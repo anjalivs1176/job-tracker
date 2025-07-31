d0baad547300e06e45ced7a329a54a0c87706cf1
anjalivs990 <anjalivs990@gmail.com>
2025-07-31 23:21:09 +0530

    Fix: Updated CORS settings

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use(cors({
 
 app.use(express.json());
 
-// Routes
+// Routes to navigate
 app.use('/api/test', testRoutes);
 app.use('/api/jobs', JobRoutes);
 app.use('/api/auth', authRoutes);
