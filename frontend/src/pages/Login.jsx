b34534d4daa9af628bbd44f68ae4d5aad03ff375
anjalivs990 <anjalivs990@gmail.com>
2025-08-01 12:27:39 +0530

    Updated auth API URLs to use deployed backend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -13,7 +13,11 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/login', { email, password });
+      const res = await axios.post(`${import.meta.env.VITE_API_BASE_URL}/api/auth/login`, {
+        email,
+        password
+    });
+
       login(res.data); // sets user in context & localStorage
       navigate('/');
     } catch (err) {
